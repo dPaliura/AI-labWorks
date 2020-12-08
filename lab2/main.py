d4d0c48c017d41e77c6496576f8ca2b645dc4651
Daniel Paliura <vinstow@gmail.com>
2020-12-08 22:56:48 +0200

    Non-valuable change

--- lab2/main.py
+++ lab2/main.py
@@ -1,4 +1,7 @@
+from numpy import array, insert
+
 from pnn import PNN 
+
 
 train_in = [[ 1,  1],
             [ 1,  2],
@@ -16,12 +19,17 @@ test_in =  [[ 1,  0],
 if __name__ == "__main__":
     my_pnn = PNN(train_in, train_out, input_names)
     recognized = my_pnn.recognize(test_in)
-    print("Test input:")
-    train = train_in
-    test = test_in
-    train_in.append(train_out)
-    test_in.append(recognized)
+    
+    train = array(train_in, dtype=str)
+    test = array(test_in, dtype=str)
+    train = insert(train, len(train[0]), array(train_out), axis=1)
+    test = insert(test, len(test[0]), array(recognized), axis=1)
+    
+
+    print("\tTRAINING INPUT")
     print(train)
+
+    print("\tTEST RECOGNITION")
     print(test)
     input()
     
